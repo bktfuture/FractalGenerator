3c2342d053c63a420c33bd32132dcc4c2869d28d
Aiturgan <aiturgan888@gmail.com>
2023-12-02 00:24:29 -0600

    add random lane width

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 	canvas.height = window.innerHeight;
 
 	// line settings
-	ctx.lineWidth = 10;
+
 	ctx.lineCap = 'round';
 	ctx.shadowColor = 'rgba(0,0,0,0.7)';
 	ctx.shadowOffsetX = 10;
@@ -14,13 +14,14 @@ window.addEventListener('load', function () {
 
 	// fractal settings/variables
 	let size = canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
-	const maxLevel = 3;
+	const maxLevel = 4;
 	const branches = 2;
 
 	let color = 'hsl(670, 100%,50%)';
 	let scale = 0.4;
 	let spread = 0.8;
 	let sides = 6;
+	let lineWidth = Math.floor(Math.random() * 20 + 10);
 
 	// controls
 	const randomizeBtn = document.getElementById('randomizeButton');
@@ -47,23 +48,28 @@ window.addEventListener('load', function () {
 			ctx.rotate(-spread);
 			drawBranch(level + 1);
 			ctx.restore();
+
 			ctx.restore();
 		}
 	}
 
 	function randomizeFractal() {
-		sides = Math.random() * 7 + 2;
-		scale = Math.random() * 0.2 + 0.6;
+		sides = Math.floor(Math.random() * 7 + 2);
+		scale = Math.random() * 0.2 + 0.4;
 		spread = Math.random() * 2.9 + 0.1;
 		color = 'hsl(' + Math.random() * 360 + ', 100%,50%)';
-		drawFractal();
+		lineWidth = Math.floor(Math.random() * 20 + 10);
 	}
 
-	randomizeBtn.addEventListener('click', randomizeFractal);
+	randomizeBtn.addEventListener('click', function () {
+		randomizeFractal();
+		drawFractal();
+	});
 
 	function drawFractal() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.strokeStyle = color;
+		ctx.lineWidth = lineWidth;
 		ctx.save();
 		ctx.translate(canvas.width / 2, canvas.height / 2);
 
